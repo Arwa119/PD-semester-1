18e258d2c126bad6da341eb0ff0f8341b1b833e6
Arwa <144407619+Arwa119@users.noreply.github.com>
2024-06-18 18:27:10 +0500

    task1.cpp

--- PD lab 3/task1.cpp
+++ PD lab 3/task1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 main(){
 	int n;
 	int angles;
-	cout<<"Enter the number of sides of the polygon: ";
+	cout<<"Enter The Number Of Sides Of The Polygon: ";
 	cin>>n;
 	angles=(n-2)*180;
 	cout<<"The sum of internal angles of a "<<n<<"-sided polygon is: "<<angles<<" degrees";
